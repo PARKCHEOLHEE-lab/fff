97f2983835ddef13b3eb6bdf1d0d7cf4e5f88d0f
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-01-18 22:49:46 +0900

    work on 'Environment object'

--- voxelate/voxelate.py
+++ voxelate/voxelate.py
@@ -5,28 +5,50 @@ import ghpythonlib.components as gh
 
 
 class Environment:
-    def __init__(self, brep):
+    def __init__(self, brep, sun_position):
         self.brep = brep
+        self.sun_position = sun_position
         self.__generate()
         
     def __generate(self):
         self.__gen_environment_bbox()
         self.__gen_hemisphere()
+        self.__gen_sun()
         
     def __gen_environment_bbox(self):
         self.environment_bbox = rg.Brep.GetBoundingBox(self.brep, rg.Plane.WorldXY)
         self.environment_bbox_faces, _, _ = gh.DeconstructBrep(self.environment_bbox)
         self.environment_bbox_bottom_face = sorted(self.environment_bbox_faces, key=lambda f: gh.Area(geometry=f).centroid.Z)[0]
+        self.environment_bbox_bottom_face_scaling, _ = gh.Scale(
+            geometry=self.environment_bbox_bottom_face,
+            center=gh.Area(self.environment_bbox_bottom_face).centroid,
+            factor=10,
+        )
         
     def __gen_hemisphere(self):
-        pass
-
+        self.sphere = gh.Sphere(
+            base=gh.Area(self.environment_bbox_bottom_face).centroid,
+            radius=100,
+        )
+        _, self.hemisphere = self.sphere.Split(self.environment_bbox_bottom_face_scaling, 0.001)
+        self.interval = rg.Interval(0, 1)
+        self.hemisphere.Faces[0].SetDomain(0, self.interval)
+        self.hemisphere.Faces[0].SetDomain(1, self.interval)
+        
+    def __gen_sun(self):
+        self.sun = self.hemisphere.Faces[0].Evaluate(
+            u=self.sun_position[0][0], 
+            v=self.sun_position[0][1], 
+            numberDerivatives=0,
+        )[1]
+        
 
 class VoxelBrep(Environment):
-    def __init__(self, brep, voxel_size, voxel_angle):
+    def __init__(self, brep, voxel_size, voxel_angle, sun_position):
         self.brep = brep
         self.voxel_size = voxel_size
         self.voxel_angle = voxel_angle
+        self.sun_position = sun_position
         self.__voxelate()
         
     def __voxelate(self):
@@ -42,7 +64,7 @@ class VoxelBrep(Environment):
         self.moved_brep_mesh = rg.Mesh.CreateFromBrep(self.moved_brep, rg.MeshingParameters(0))
         
     def __gen_environment(self):
-        Environment.__init__(self, self.moved_brep)
+        Environment.__init__(self, self.moved_brep, sun_position)
         
     def __gen_plane(self):
         self.plane = gh.RotatePlane(plane=rg.Plane.WorldXY, angle=self.voxel_angle)
@@ -115,6 +137,12 @@ class VoxelBrep(Environment):
 
 
 if __name__ == "__main__":
-    voxel_brep = VoxelBrep(brep=brep, voxel_size=2.5, voxel_angle=math.radians(degree))
+    voxel_brep = VoxelBrep(
+        brep=brep, 
+        voxel_size=2.5, 
+        voxel_angle=math.radians(degree), 
+        sun_position=sun_position
+    )
+    
     voxels = voxel_brep.voxels_mesh
-    a = voxel_brep.environment_bbox
+    a = voxel_brep.sun, voxel_brep.hemisphere
