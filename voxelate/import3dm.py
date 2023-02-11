38f40c86c463e556ce3047a57c6fd2a771f8e70b
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2023-02-11 17:11:58 +0900

    add VoxelUnits and VoxelConditions resolve issue that related intersection parapet edges

--- voxelate/import3dm.py
+++ voxelate/import3dm.py
@@ -11,12 +11,26 @@ if __name__ == "__main__":
     model_3dm = [m for m in os.listdir(MODEL_PATH) if m[-3:] == "3dm"]
     unit_3dm = [u for u in os.listdir(UNIT_PATH) if u[-3:] == "3dm"]
     
-    _79andpark_3dm = model_3dm[0]
-    _legotowers_3dm = model_3dm[1]
-    _red7_3dm = model_3dm[2]
-    _vancouverhouse_3dm = model_3dm[3]
+    (
+        _79andpark_3dm, 
+        _legotowers_3dm, 
+        _red7_3dm, 
+        _vancouverhouse_3dm,
+    ) = model_3dm
     
-    _exterior_unit_3dm = unit_3dm[0]
+    (
+        _exterior_unit_3dm, 
+        _exterior_both_unit_3dm, 
+        _exterior_corner_unit_3dm, 
+        _exterior_corner_o_unit_3dm,
+        _exterior_corner_u_unit_3dm,
+        _
+    ) = unit_3dm
     
-    model = gh.Import3DM(os.path.join(MODEL_PATH, _red7_3dm), "*", "*")
+    model = gh.Import3DM(os.path.join(MODEL_PATH, _vancouverhouse_3dm), "*", "*")
+    
     exterior_unit = gh.Import3DM(os.path.join(UNIT_PATH, _exterior_unit_3dm), "*", "*")
+    exterior_both_unit = gh.Import3DM(os.path.join(UNIT_PATH, _exterior_both_unit_3dm), "*", "*")
+    exterior_corner_unit = gh.Import3DM(os.path.join(UNIT_PATH, _exterior_corner_unit_3dm), "*", "*")
+    exterior_corner_o_unit = gh.Import3DM(os.path.join(UNIT_PATH, _exterior_corner_o_unit_3dm), "*", "*")
+    exterior_corner_u_unit = gh.Import3DM(os.path.join(UNIT_PATH, _exterior_corner_u_unit_3dm), "*", "*")
